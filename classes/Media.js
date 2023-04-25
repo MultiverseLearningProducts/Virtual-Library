465175ba7471de695b88cda96a1640e6c0ca2944
nimonian <nimon@hotmail.co.uk>
2023-04-25 15:23:19 +0100

    deletes solution

--- classes/Media.js
+++ classes/Media.js
@@ -1,19 +1,4 @@
 // create your Media class:
 
-class Media {
-  static totalMediaCount = 0
-
-  constructor(title, year, genre) {
-    this.title = title
-    this.genre = genre
-    this.year = year
-    Media.totalMediaCount++
-  }
-
-  summary() {
-    return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`
-  }
-}
-
 // uncomment below to export it:
 module.exports = { Media }
