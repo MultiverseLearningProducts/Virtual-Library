b5eaf0430d98e4a2697c759e52cd990ee499dfc1
Ycandido0119 <yassircandido2@gmail.com>
2023-07-26 11:43:15 +0100

    First commit

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,15 @@
-// create your Media class:
+class media {
+    constructor(title, year, genre) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+    }
+    static totalMediaCount;
+    
+    summary() {
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre} `
+    }
+}
 
 // uncomment below to export it:
-module.exports = { Media }
+module.exports = {media}
