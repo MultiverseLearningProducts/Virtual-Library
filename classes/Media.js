39ed7b0200244bfbee005066e93f66d888ff38d5
Bo <26774196+cpu-byte@users.noreply.github.com>
2023-10-21 19:28:31 +0100

    adding summary() method and demo of functionality

--- classes/Media.js
+++ classes/Media.js
@@ -1,23 +1,41 @@
 class Media {
-  // Static property to keep track of the instance count
+  // Static property to keep track of the instance count of Media class
   static totalMediaCount = 0;
 
+  /**
+   * Create a piece of media
+   * @param {string} title
+   * @param {number} year
+   * @param {string} genre
+   */
   constructor(title, year, genre) {
     this.title = title;
     this.year = year;
     this.genre = genre;
+
     // Increment the instance count each time a new instance is created
     Media.totalMediaCount++;
   }
+
+  summary() {
+    return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`;
+  }
 }
 
-// console.log({ "Media.totalMediaCount: ": Media.totalMediaCount });
-// const book = new Media("Cool Piece of Media", 1951, "Fiction");
-// console.log({ book, "Media.totalMediaCount: ": Media.totalMediaCount });
+// demonstrating initial value of media count
+console.log("Media.totalMediaCount: ", Media.totalMediaCount);
 
-// const music = new Media("Abbey Road", 1969, "Rock");
-// console.log({ music, "Media.totalMediaCount: ": Media.totalMediaCount });
-// music.summary(); // "Title: Abbey Road, Year: 1969, Genre: Rock"
+// adding a piece of media
+const book = new Media("Cool Piece of Media", 2023, "Fiction");
+console.log("book:", book);
+console.log("Media.totalMediaCount: ", Media.totalMediaCount);
 
-// uncomment below to export it:
+// adding another piece of media
+const music = new Media("Abbey Road", 1969, "Rock");
+console.log("music:", music);
+console.log("Media.totalMediaCount: ", Media.totalMediaCount);
+
+// summary demonstration
+console.log("music.summary():", music.summary());
+
 module.exports = Media;
