9e388f872c936ef8abbf8c5689af7465baf0377c
LoisClinton <loisclinton@hotmail.co.uk>
2023-10-21 19:45:37 +0100

    added book and music

--- classes/Book.js
+++ classes/Book.js
@@ -1,5 +1,16 @@
 // import the Media class:
 
+class Book extends Media {
+  constructor(title, year, genre, author, numPages, rating) {
+    super(title, year, genre);
+    this.author = author;
+    this.numPages = numPages;
+    this.rating = rating;
+  }
+  highestRating(books) {
+    return;
+  }
+}
 // create your Book class:
 
 // don't change below
--- classes/Music.js
+++ classes/Music.js
@@ -1,5 +1,14 @@
 // import the Media class:
+class Music extends Media {
+  constructor(title, year, genre, artist, length) {
+    this.artist = artist;
+    this.length = length;
+  }
 
+  highestRating(books) {
+    return;
+  }
+}
 // create your Music class:
 
 // don't change below
