b51beb8e9d7b5b31959478f0cb59a49458cbb73a
Bo <26774196+cpu-byte@users.noreply.github.com>
2023-10-21 19:20:29 +0100

    Media class with totalMediaCount

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,23 @@
-// create your Media class:
+class Media {
+  // Static property to keep track of the instance count
+  static totalMediaCount = 0;
+
+  constructor(title, year, genre) {
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+    // Increment the instance count each time a new instance is created
+    Media.totalMediaCount++;
+  }
+}
+
+// console.log({ "Media.totalMediaCount: ": Media.totalMediaCount });
+// const book = new Media("Cool Piece of Media", 1951, "Fiction");
+// console.log({ book, "Media.totalMediaCount: ": Media.totalMediaCount });
+
+// const music = new Media("Abbey Road", 1969, "Rock");
+// console.log({ music, "Media.totalMediaCount: ": Media.totalMediaCount });
+// music.summary(); // "Title: Abbey Road, Year: 1969, Genre: Rock"
 
 // uncomment below to export it:
 module.exports = Media;
