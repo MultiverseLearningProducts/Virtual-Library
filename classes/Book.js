4054ec8b2ac821516bbf35fe7927ef935ee6c318
Willxones <william.jones@hl.co.uk>
2023-10-24 13:03:40 +0100

    Media and Half of books added

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,21 @@
 // import the Media class:
-
+const Media = require('./classes/Media.js')
 // create your Book class:
-
+class Book extends Media {
+    constructor(author, numPages, rating, title, year, genre) {
+        this.author = author
+        this.numPages = numPages
+        this.rating = rating
+        super(title, year, genre)
+    }
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+    static highestRating(books) {
+        for (let rating of books) {
+            
+        }
+    }
+}
 // don't change below
 module.exports = Book;
