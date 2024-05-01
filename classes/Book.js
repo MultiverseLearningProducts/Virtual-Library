664696485bdad5601d06e0e0df519c36effe7570
Andrii-Mysheniuk <amysheniuk@webfx.co.uk>
2024-05-01 12:03:50 +0100

    add task solution

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,29 @@
 // import the Media class:
-
+const Media = require('./Media');
 // create your Book class:
+class Book extends Media {
+  constructor(title, year, genre, author, numPages, rating) {
+    super(title, year, genre);
+    this.author = author;
+    this.numPages = numPages;
+    this.rating = rating;
+  }
 
+  static highestRating(books) {
+    let highest = books[0];
+
+    for (let i = 1; i < books.length; i++) {
+      if (books[i].rating > highest.rating) {
+        highest = books[i];
+      }
+    }
+
+    return highest;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+}
 // don't change below
 module.exports = Book;
