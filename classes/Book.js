7bde41695d0baf5dc77c3ac9d94cfc6f047e2345
JFMcFaull <jfmcfaull@gmail.com>
2024-05-08 19:52:09 +0100

    finished

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,32 @@
 // import the Media class:
-
+const Media = require('/Users/jamiemcfaull/Virtual-Library/classes/Media.js')
 // create your Book class:
 
+class Book extends Media {
+    constructor(year, genre, title, author, numPages, rating){
+        super (year, genre, title )
+        this.author = author
+        this.numPages = numPages
+        this.rating = rating
+    }
+
+    summary (title, author, year, numPages, genre, rating){
+        return ("Title: " + this.title + ", Author: " + this.author + ", Year: " + this.year + ", Page Count: " + this.numPages + ", Genre: " + this.genre + ", Rating: " + this.rating)
+        }
+
+    static highestRating(books) {
+        if (!books || !books.length) {
+            return null;
+        }
+    
+        const highestRatingIndex = books.reduce((currentIndex, book, index) => {
+            return book.rating > books[currentIndex].rating ? index : currentIndex;
+        }, 0);
+    
+        return books[highestRatingIndex];
+        }
+}
+   
+   
 // don't change below
 module.exports = Book;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,32 @@
 // import the Media class:
+const Media = require('/Users/jamiemcfaull/Virtual-Library/classes/Media.js')
 
 // create your Music class:
+class Music extends Media {
+    constructor (title, genre, year, artist, length) {
+        super (title, genre, year)
+        this.artist = artist
+        this.length = length 
+    }
+
+
+    summary () {
+        return ("Title: " + this.title + ", Artist: " + this.artist + ", Year: " + this.year + ", Genre: " + this.genre + ", Length: " + this.length + " seconds") 
+    }
+
+    static shortestAlbum(music) {
+        if (!music || !music.length) {
+          return null;
+        }
+      
+        const shortestAlbumIndex = music.reduce((currentIndex, album, index) => {
+          return album.length < music[currentIndex].length ? index : currentIndex; 
+        }, 0);
+      
+        return music[shortestAlbumIndex];
+      }
+}
+
 
 // don't change below
 module.exports = Music;
