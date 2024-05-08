0d11ebe42bb83d6d930fd7d56678ea11dcbb14a5
J3nbuu <jenbu3d@gmail.com>
2024-05-08 17:31:38 +0100

    code added - n2a - static on movie and test

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,31 @@
 // import the Media class:
-
+const Media = require('./classes/Media'); 
 // create your Book class:
+
+class Book extends Media{
+    constructor(title, author, year, genre, numPages, rating){
+        super(title, year, genre)
+        this.author = author
+        this.numPages = numPages
+        this.rating = rating
+    }
+
+    summary(){
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+
+    static highestRating(bookArray) {
+        if (bookArray.length === 0) return null;
+    
+        let highestRatedBook = bookArray[0];
+        for (let i = 1; i < bookArray.length; i++) {
+            if (bookArray[i].rating > highestRatedBook.rating) {
+                highestRatedBook = bookArray[i];
+            }
+        }
+        return highestRatedBook;
+    }
+}
 
 // don't change below
 module.exports = Book;
--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,21 @@
 // create your Media class:
+//constructor houses the attributes
 
+class Media{
+    constructor(title, year, genre,){
+        this.title = title
+        this.year = year
+        this.genre = genre
+        Media.totalMediaCount++
+    }
+
+    //method always add () then {}
+    summary(){
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`
+    }
+}
+
+
+Media.totalMediaCount = 0;
 // uncomment below to export it:
 module.exports = Media;
