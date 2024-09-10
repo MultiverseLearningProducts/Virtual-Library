9a6326b2d4a49f9739d27d4c9fe3e820ac5a70be
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-09-10 16:43:42 -0400

    Created Media & Book Classes

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,28 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Book class:
+class Book extends Media {
+  constructor(title, author, numPages, genre, rating) {
+    super(title, genre);
+    this.author = author;
+    this.numPages = numPages;
+
+    if (rating < 1 || rating > 5) {
+      throw new Error("Rating must be betwenn 1 and 5.");
+    }
+    this.rating = rating;
+  }
+  summary() {
+    return `"Title: this.title, Author: this.author, Year: this.year, Page Count: this.numPages, Genre: this.genre, Rating: this.rating`;
+  }
+  static highestRating(books) {
+    return books.reduce(
+      (highest, book) =>
+        book.rating > (highest?.rating || 0) ? book : highest,
+      null
+    );
+  }
+}
 
 // don't change below
 module.exports = Book;
--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,23 @@
 // create your Media class:
+class Media {
+  static totalMediaCount = 0;
+  constructor(title, year, genre) {
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+
+    Media.totalMediaCount++;
+  }
+  summary() {
+    return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`;
+  }
+  static getTotalMediaCount() {
+    return Media.totalMediaCount;
+  }
+}
 
 // uncomment below to export it:
+const book = new Media("The Catcher in the Rye", 1951, "Fiction");
+console.log(book.summary());
+console.log(Media.getTotalMediaCount());
 module.exports = Media;
